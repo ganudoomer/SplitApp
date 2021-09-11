abccfaf7bd351ba31f1bc969944dd7a2256ff455
ganudoomer <ganuiscool@gmail.com>
2021-09-11 23:51:45 +0530

    Initial Commit

---
+++ SplitApp/SplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitAppApp.swift
+//  SplitApp
+//
+//  Created by Sree on 11/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
